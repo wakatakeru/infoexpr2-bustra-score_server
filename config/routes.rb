f7cda9a71abd078d2d1a43820bdc01ee1637fe1d
wakatakeru <s15t233@stu.kagawa-u.ac.jp>
2017-11-16 18:50:12 +0900

    ルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  get 'scores/index'
-
-  get 'scores/create'
+  get  'scores/index'
+  post 'scores/create'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
