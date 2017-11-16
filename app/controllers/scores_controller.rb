9bb517bc446eb15d2b4a9f937e897f15d37cd160
wakatakeru <s15t233@stu.kagawa-u.ac.jp>
2017-11-16 18:58:21 +0900

    コントローラとしての挙動を定義

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -1,7 +1,14 @@
 class ScoresController < ApplicationController
   def index
+    @scores = Score.order('point DESC')
   end
 
   def create
+    score = Score.new
+
+    score.username = params['username']
+    score.point    = params['point'].to_i
+
+    score.save
   end
 end
